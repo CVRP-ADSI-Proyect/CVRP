4383776656b88711f187b9bc91575559371a982a
Sebastian Triana <53375205+sebastian-triana@users.noreply.github.com>
2021-06-12 17:19:22 -0500

    Chore: se actualiza frontend

--- 03-Desarrollo/cvrp.bkp/resources/views/Landing/categorias.blade.php
+++
@@ -1,3 +0,0 @@
-<h1>prueba categorias</h1>
-
-<a href="{{route('landing.home')}}">Inicio</a> || <a href="">Acerca de</a> || <a href="">Contacto</a>
---
+++ 03-Desarrollo/cvrp_v.2 - copia/resources/views/footer/footer.blade.php
@@ -0,0 +1,37 @@
+<div  id="footerSection">
+	<div class="container">
+		<div class="row">
+			<div class="span3">
+				<h5>CUENTA</h5>
+				<a href="{{route('login')}}">MI CUENTA</a>
+				<a href="{{route('login')}}">INFORMACION PERSONAL</a> 
+				<a href="{{route('login')}}">DIRECCIONES</a> 
+				<a href="{{route('login')}}">DESCUENTOS</a>  
+				<a href="{{route('login')}}">HISTORIAL DE COMPRAS</a>
+			 </div>
+			<div class="span3">
+				<h5>INFORMACION</h5>
+				<a href="contact.html">CONTACTO</a>  
+				<a href="register.html">REGISTRO</a>  
+				<a href="legal_notice.html">AVISO LEGAL</a>  
+				<a href="tac.html">TERMINOS Y CONDICIONES</a> 
+				<a href="faq.html">FAQ</a>
+			 </div>
+			<div class="span3">
+				<h5>NUESTRAS OFERTAS</h5>
+				<a href="#">NUEVOS PRODUCTOS</a> 
+				<a href="#">LOS MÁS VENDIDOS</a>  
+				<a href="{{route ('offer')}}">OFERTAS ESPECIALES</a>  
+				<a href="#">MANUFACTURERS</a> 
+				<a href="#">SUPPLIERS</a> 
+			 </div>
+			<div id="socialMedia" class="span3 pull-right">
+				<h5>REDES SOCIALES</h5>
+				<a href="#"><img width="60" height="60" src="themes/images/facebook.png" title="facebook" alt="facebook"/></a>
+				<a href="#"><img width="60" height="60" src="themes/images/twitter.png" title="twitter" alt="twitter"/></a>
+				<a href="#"><img width="60" height="60" src="themes/images/youtube.png" title="youtube" alt="youtube"/></a>
+			 </div> 
+		 </div>
+		<p class="pull-right">&copy; CVRP</p>
+	</div><!-- Container End -->
+	</div>
