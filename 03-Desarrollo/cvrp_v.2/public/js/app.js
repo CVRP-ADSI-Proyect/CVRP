2a3b9c50e8f11dad170e2d9303d01806e58ee4bd
Sebastian Triana <53375205+sebastian-triana@users.noreply.github.com>
2021-06-12 10:55:23 -0500

    Chore: se actualiza front-end

---
+++ 03-Desarrollo/cvrp_v.2/public/js/app.js
@@ -0,0 +1,35 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/js/app.js":
+/*!*****************************!*\
+  !*** ./resources/js/app.js ***!
+  \*****************************/
+/***/ (() => {
+
+require('./bootstrap', '/bootshop', '/bootstrap.min', '/jquery', '/jquery.lightbox-0.5', '/less', '/prettify');
+
+/***/ }),
+
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/css-loader/dist/cjs.js):\nError: Can't resolve '../images/headerRepeat.png' in 'S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\resources\\sass'\n    at finishWithoutResolve (S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\enhanced-resolve\\lib\\Resolver.js:293:18)\n    at S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\enhanced-resolve\\lib\\Resolver.js:362:15\n    at S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\enhanced-resolve\\lib\\Resolver.js:410:5\n    at eval (eval at create (S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:16:1)\n    at S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\enhanced-resolve\\lib\\Resolver.js:410:5\n    at eval (eval at create (S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:27:1)\n    at S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\enhanced-resolve\\lib\\DescriptionFilePlugin.js:87:43\n    at S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\enhanced-resolve\\lib\\Resolver.js:410:5\n    at eval (eval at create (S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:15:1)\n    at S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\enhanced-resolve\\lib\\Resolver.js:410:5\n    at processResult (S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\webpack\\lib\\NormalModule.js:703:19)\n    at S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\webpack\\lib\\NormalModule.js:809:5\n    at S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\loader-runner\\lib\\LoaderRunner.js:399:11\n    at S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\loader-runner\\lib\\LoaderRunner.js:251:18\n    at context.callback (S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at Object.loader (S:\\xampp\\htdocs\\CVRP\\03-Desarrollo\\cvrp_v.2\\node_modules\\css-loader\\dist\\index.js:155:5)\n    at processTicksAndRejections (internal/process/task_queues.js:95:5)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	__webpack_modules__["./resources/js/app.js"]();
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./resources/sass/app.scss"]();
+/******/ 	
+/******/ })()
+;
---
+++ 03-Desarrollo/cvrp_v.2/resources/js/app.js
@@ -0,0 +1,1 @@
+require('./bootstrap','/bootshop','/bootstrap.min','/jquery','/jquery.lightbox-0.5','/less','/prettify');
